38517ce58727fd213749f180f4c7aa000a9fec09
Johan Mesa <Johanotr@gmail.com>
2025-10-29 10:37:41 -0400

    chore: import req and res types

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,3 +1,5 @@
-const getUser = async (req, res) => {
+import type { Request, Response } from "express";
+
+const getUser = async (req: Request, res: Response) => {
     res.send(200).json({msg:"User"});
 }
