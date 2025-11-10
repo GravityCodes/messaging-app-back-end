1fc5094c2c231cf342bbb1421b8e196ec329e632
Johan Mesa <Johanotr@gmail.com>
2025-11-10 16:28:58 -0500

    feat: add cors and socket

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io"
 
 const port = process.env.PORT || 3000;
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:5173"]
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "okay" });
 });
 
-app.listen(port, () => console.log("Listening on port:", port));
+io.on("connection", (socket) => {
+  console.log('a user connected');
+}); 
+
+httpServer.listen(port, () => console.log("Listening on port:", port));
