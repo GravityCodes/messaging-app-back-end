d6af7ee1866605d57f648312ba119c0f91cf34de
Johan Mesa <Johanotr@gmail.com>
2025-12-16 12:27:09 -0500

    feat: start loging and signup routes

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -4,4 +4,14 @@ const getUser = async (req: Request, res: Response) => {
   res.send(200).json({ msg: "User" });
 };
 
-export default { getUser };
+const loginUser = async(req: Request, res: Response) => {
+    //Login user.
+}
+
+const signupUser = async(req: Request, res: Response) => {
+  //Sign Up User
+}
+
+
+
+export default { getUser, loginUser, signupUser };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
-import { Server } from "socket.io"
+import { Server } from "socket.io";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -12,17 +12,18 @@ app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173"]
-  }
+    origin: ["http://localhost:5173"],
+  },
 });
-
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "okay" });
 });
 
+
 io.on("connection", (socket) => {
-  console.log('a user connected');
-}); 
+  console.log("a user connected");
+});
+
 
 httpServer.listen(port, () => console.log("Listening on port:", port));
