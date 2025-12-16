7f7f93df2f7e4546caffeea785f149c83aa6b65e
Johan Mesa <Johanotr@gmail.com>
2025-12-16 14:40:31 -0500

    feat: add createUser validation

---
+++ src/validator/user.ts
@@ -0,0 +1,38 @@
+import { body } from "express-validator";
+import { PrismaClient } from "../generated/prisma/client.js";
+const prisma = new PrismaClient();
+
+const createUser = [
+    body("userName")
+    .trim()
+    .notEmpty()
+    .withMessage("User name must not be empty"),
+    body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("User name must not be empty")
+    .isEmail()
+    .withMessage("Email has incorrect format")
+    .custom(async (value) => {
+        const result = await prisma.user.findUnique({
+            where: {email: value}
+        })
+
+        if(result){
+            throw new Error("E-mail already in use");
+        }
+    }),
+    body("password")
+    .notEmpty()
+    .withMessage("Password must not be empty")
+    .isStrongPassword()
+    .withMessage("Password is not strong enough"),
+    body("passwordConfirmation")
+    .notEmpty()
+    .custom((value, {req}) => {
+        return value === req.body.password;
+    })
+    .withMessage("Passwords must match")
+]
+
+export {createUser}
