96deda84da7ac6294772a35a54dcd9a34605938f
Johan Mesa <Johanotr@gmail.com>
2025-12-17 17:44:37 -0500

    feat: create password hash util

---
+++ src/utils/password.ts
@@ -0,0 +1,16 @@
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+
+async function hashPassword(password: string){
+    try{
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        return hashedPassword;
+
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+export {hashPassword}
