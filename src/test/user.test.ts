da1edfdfb5e37e540db9a2b8129d904d9c3b4d0c
Johan Mesa <Johanotr@gmail.com>
2025-12-23 17:12:05 -0500

    feat: configure jest

---
+++ src/test/user.test.ts
@@ -0,0 +1,19 @@
+import app from "../app";
+import express from "express";
+// import request from "supertest";
+
+const newUser = {
+    userName: "JohanM",
+    email: "Johan.mesa2001@gmail.com",
+    password: "Pa$$word#1",
+    passwordConfirmation: "Pas$$word#1"
+}
+
+describe("User Route", () => {
+}
+
+test('object assignment', () => {
+    expect(2+2).toBe(4);
+});
+
+
