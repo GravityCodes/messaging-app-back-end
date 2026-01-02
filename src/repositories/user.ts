5bc453162c578fe5e9f9f25cbced678a83189905
Johan Mesa <Johanotr@gmail.com>
2026-01-01 21:35:20 -0500

    feat: add user repository class to decouple data access

---
+++ src/repositories/user.ts
@@ -0,0 +1,22 @@
+import { prisma } from "../lib/prisma.js";
+import type { User } from "../generated/prisma/client.js";
+
+class Repository {
+  async createUser(
+    userName: string,
+    email: string,
+    hashedPassword: string,
+  ): Promise<User | null> {
+    const user = await prisma.user.create({
+      data: {
+        userName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  }
+}
+
+export default Repository;
