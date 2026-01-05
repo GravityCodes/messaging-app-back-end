9ea863f4cd40e4117b697f7600da229eaa650db2
Johan Mesa <Johanotr@gmail.com>
2026-01-05 16:57:54 -0500

    fix: add next function to validation middleware

--- src/middleware/validationResults.ts
+++ src/middleware/validationResults.ts
@@ -1,11 +1,13 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-export const validateRequest = (req: Request, res: Response) => {
+export const validateRequest = (req: Request, res: Response, next: NextFunction) => {
   const result = validationResult(req);
 
   if (!result.isEmpty()) {
     console.error(result.array());
     return res.status(400).json({ errors: result.array() });
   }
+
+  next();
 };
