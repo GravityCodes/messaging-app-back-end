ea3bdec2432598ec48d45efe587fe6b2ed95b5a3
Johan Mesa <Johanotr@gmail.com>
2026-01-06 10:05:39 -0500

    add cookieparser to express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
