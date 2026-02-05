0ebab843e881329de1eb5cc011f0dfaad50a8057
Johan Mesa <Johanotr@gmail.com>
2026-02-05 08:28:14 -0500

    feat: create update user repository function

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,7 +1,32 @@
 import { prisma } from "../lib/prisma.js";
 import type { User } from "../generated/prisma/client.js";
+import { createClient } from "@supabase/supabase-js";
 
 export class Repository {
+  #supabase = createClient(
+    `${process.env.SUPABASE_PROJECT_URL}`,
+    `${process.env.SUPABASE_API_KEY}`,
+  );
+
+  async #uploadFile(
+    file: Buffer,
+    userId: number,
+    fileName: string,
+    fileType: string,
+  ) {
+    const { data, error } = await this.#supabase.storage
+      .from("messaging-app")
+      .upload(`users/${userId}/${fileName}.${fileType}`, file);
+    if (error) {
+      throw new Error(`${error}`);
+    } else {
+      const { data } = this.#supabase.storage
+        .from("messaging-app")
+        .getPublicUrl(`users/${userId}/${fileName}.${fileType}`);
+      return data.publicUrl;
+    }
+  }
+
   async createUser(
     userName: string,
     email: string,
@@ -22,6 +47,57 @@ export class Repository {
     const user = await prisma.user.findUnique({
       where: {
         email,
+      },
+    });
+
+    return user;
+  }
+
+  async updateUser(
+    profileImage: Express.Multer.File | undefined,
+    bannerImage: Express.Multer.File | undefined,
+    userName: string | undefined,
+    id: number,
+  ): Promise<User | null> {
+    let profileImageUrl = "";
+    let bannerImageUrl = "";
+
+    if (profileImage !== undefined) {
+      profileImageUrl = await this.#uploadFile(
+        profileImage.buffer,
+        id,
+        "profile",
+        profileImage.mimetype.slice(6),
+      );
+    }
+
+    if (bannerImage !== undefined) {
+      bannerImageUrl = await this.#uploadFile(
+        bannerImage.buffer,
+        id,
+        "banner",
+        bannerImage.mimetype.slice(6),
+      );
+    }
+
+    const user = await prisma.user.update({
+      where: { id },
+      data: {
+        ...(profileImage !== undefined
+          ? {
+              profilePicture: profileImageUrl,
+            }
+          : {}),
+        ...(bannerImage !== undefined
+          ? {
+              banner: bannerImageUrl,
+            }
+          : {}),
+        ...(userName !== undefined
+          ? {
+              userName,
+            }
+          : {}),
       },
     });
 
