a37d28dde2faec243c34e9ad2b79cb357a69c481
Johan Mesa <Johanotr@gmail.com>
2026-02-08 11:18:11 -0500

    feat: add updateUser function to controller

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from "express";
-import { Service } from "../services/user";
-import { Repository } from "../repositories/user";
+import { Service } from "../services/user.js";
+import { Repository } from "../repositories/user.js";
 
 const service = new Service(new Repository());
 
 const getUser = async (req: Request, res: Response) => {
-  res.send(200).json({ msg: "User" });
+  return res.status(200).json({ msg: "User" });
 };
 
 const loginUser = async (req: Request, res: Response) => {
@@ -56,4 +56,31 @@ const signupUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { getUser, loginUser, signupUser };
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    let { userName } = req.body;
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    const profileImage = files?.["profile"]?.[0] || undefined;
+    const bannerImage = files?.["banner"]?.[0] || undefined;
+
+    if (userName == req.user.userName) {
+      userName = undefined;
+    }
+
+    const user = await service.updateUserProfile(
+      profileImage,
+      bannerImage,
+      userName,
+      req.user.id,
+    );
+
+    return res.status(201).json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      errors: "An internal server error has occured. Please try again later.",
+    });
+  }
+};
+
+export default { getUser, loginUser, signupUser, updateUser };
